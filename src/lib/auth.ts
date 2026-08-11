fc0f3b3f7996d0a964804230a380d8284946d2ed
Arthur Moraes <arthur.m.thurler@gmail.com>
2026-08-11 20:35:05 -0300

    chore: prepare application for production environment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,16 +5,18 @@ import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     secret: process.env.BETTER_AUTH_SECRET,
-    baseURL: process.env.BETTER_AUTH_URL ?? process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000", 
+    baseURL:
+        process.env.BETTER_AUTH_URL ??
+        process.env.NEXT_PUBLIC_APP_URL ??
+        "http://localhost:3000",
     database: prismaAdapter(prisma, {
-        provider: 'postgresql'
+        provider: "postgresql",
     }),
-    emailAndPassword: { 
-        enabled: true, 
-      },
-      plugins: [nextCookies()],
-      trustedOrigins: [
-        process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000"
-      ],
-
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [nextCookies()],
+    trustedOrigins: [
+        process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+    ],
 });
